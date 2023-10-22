66b0a310e7152a5ee66a97c173a1673ba037d10b
lixyleon <123250641+lixyleon@users.noreply.github.com>
2023-10-22 17:48:00 -0400

    Create footer in php page Create footer in page with php extension and include it in the index. The colors of the footer are modified from white to purple and pink.

--- index.php
+++ index.php
@@ -239,48 +239,6 @@ include("template/navbar.php");
     </div>
   </div>
 
-  <br><br><br>
-  <div class="container">
-    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-      <p class="col-md-4 mb-0 text-body-secondary">© 2023 Creaciones LiTh</p>
-  
-      <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-      </a>
-  
-      <ul class="nav col-md-4 justify-content-end">
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Inicio</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Catálogo</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Nosotros</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Contactos</a></li>
-      </ul>
-    </footer>
-  </div>
-
-  <!-- logo <a href="https://www.freepik.es/foto-gratis/marco-circulo-femenino_4191308.htm#query=logo&position=44&from_view=search&track=sph">Imagen de rawpixel.com</a> en Freepik
-  
-  <a href="https://www.freepik.es/foto-gratis/taza-bebida-barra-chocolate-panecillo-sobre-trozo-madera_6144222.htm#query=tazas%20decoradas&position=13&from_view=search&track=ais">Imagen de azerbaijan_stockers</a> en Freepik
-
-Imagen de <a href="https://www.freepik.es/foto-gratis/fondo-pantalla-optimismo-cara-sonriente_36029122.htm#query=tazas%20decoradas&position=11&from_view=search&track=ais">Freepik</a>
-
-<a href="https://www.freepik.es/foto-gratis/caja-regalo-renderizado-3d-paquete-regalo-cinta_33525360.htm#query=regalo&position=1&from_view=search&track=sph">Imagen de upklyak</a> en Freepik
-
-<a href="https://www.freepik.es/foto-gratis/limpie-taza-cafe-leche-sobre-fondo-blanco_54037692.htm#query=taza%20blanca&position=29&from_view=search&track=ais">Imagen de Vectonauta</a> en Freepik
-
-Imagen de <a href="https://www.freepik.es/foto-gratis/vista-frontal-muchos-corazones-rosados-que-salen-caja-actual_15857216.htm#query=regalos&position=34&from_view=search&track=sph">Freepik</a>
-
-<a href="https://www.freepik.es/foto-gratis/composicion-camiseta-blanca-lisa-bolso-mano_67573342.htm#query=sublimaci%C3%B3n&position=5&from_view=search&track=sph">Imagen de Vectonauta</a> en Freepik
-
-Imagen de <a href="https://www.freepik.es/vector-gratis/fondo-abstracto-estilo-papel_64787254.htm#page=2&query=fondos%20para%20paginas%20web&position=38&from_view=search&track=ais">Freepik</a>
-Imagen de <a href="https://www.freepik.es/vector-gratis/fondo-realista-rosa-plata_35575905.htm#page=17&query=fondos%20para%20paginas%20web&position=2&from_view=search&track=ais">Freepik</a>
-Imagen de <a href="https://www.freepik.es/foto-gratis/fondo-pantalla-optimismo-cara-sonriente_36029122.htm#query=tazas%20blancas&position=35&from_view=search&track=ais">Freepik</a>-->
-  
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
-    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
-    crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
-    integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
-    crossorigin="anonymous"></script>
-</body>
-
-</html>
+  <?php 
+include("template/footer.php");
+?>
