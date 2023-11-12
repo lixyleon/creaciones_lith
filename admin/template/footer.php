a3c016162c27f9487bec26e961d64628be71cd41
lixyleon <123250641+lixyleon@users.noreply.github.com>
2023-11-12 14:56:20 -0400

    create template create navbar and footer of administrator section

---
+++ admin/template/footer.php
@@ -0,0 +1,14 @@
+
+<br><br>
+<div class="container-fluid fixed-bottom" >
+<footer class="py-3 my-4 border-top border-dark-subtle">
+    <p class="text-center" >© 2023 Creaciones LiTh</p>
+  </footer>
+</div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+</body>
+</body>
+</html>
